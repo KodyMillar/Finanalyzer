f57af14323add8ba6b5133b36f21350cf5b8d857
Kody <kodymillar@hotmail.com>
2024-10-14 15:08:16 -0700

    Creates receiver of data from database and periodically checks database for updates

--- show-results/frontend/src/App.js
+++ show-results/frontend/src/App.js
@@ -5,28 +5,50 @@ import { getUserFinanceData } from './api/userFinanceData';
 
 function App() {
 
-  const [userFinanceData, setUserFinanceData] = useState({
-    investments: [
-      {
-        interest: 0,
-        interestPercentage: 0
-      }
-    ]
-  });
+  const [userFinanceData, setUserFinanceData] = useState({ investments: [] });
+  const [currentInvestments, setCurrentInvestments] = useState([]);
+
+  useEffect(() => {
+    const checkForUpdates = () => {
+      getUserFinanceData()
+        .then((response) => {  
+          if (response.status === 200) {
+            response.data.sort((a, b) => a._id.localeCompare(b._id));
+            currentInvestments.sort((a, b) => a._id.localeCompare(b._id));
+    
+            const new_investments = []
+            for (let i=0; i < response.data.length; i++) {
+              if (currentInvestments.length === 0 || !(response.data[i]._id === currentInvestments[i]?._id)) {      
+                new_investments.push(response.data[i]);
+              }
+            }
+  
+            if (new_investments.length > 0) {
+              setCurrentInvestments((prev) => {
+                return [...new_investments, ...prev];
+              });
+            }
+  
+          }
+        })
+        .catch((err) => console.error(err));
+    } 
+
+    checkForUpdates()
+    const intervalId = setInterval(checkForUpdates, 30000);
+
+    return () => clearInterval(intervalId);
+  }, [currentInvestments])
   
   useEffect(() => {
-    getUserFinanceData()
-      .then((response) => {
-        if (response.status == 200) {
-          setUserFinanceData(() => {
-            return {
-              investments: response.data
-            }
-          });
-        }
-      })
-      .catch((err) => console.error(err));
-  }, []);
+    setUserFinanceData(() => ({
+      investments: [
+        ...currentInvestments
+      ]
+    }));
+
+  }, [currentInvestments])
+
   
   return (
     <>
